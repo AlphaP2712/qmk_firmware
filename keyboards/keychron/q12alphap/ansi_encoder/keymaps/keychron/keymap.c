c645b8d96c16758b8d8fad8a6f4b9c3cae8e9391
AlphaP <Putthinart.A@gmail.com>
2023-03-29 08:10:06 +0700

    Update keymap.c

--- keyboards/keychron/q12alphap/ansi_encoder/keymaps/keychron/keymap.c
+++ keyboards/keychron/q12alphap/ansi_encoder/keymaps/keychron/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P1,    KC_P2,    KC_P3,    KC_PENT,   KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,      KC_COMM, KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
         KC_P0,              KC_PDOT,             KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
     [WIN_FN] = LAYOUT_ansi_103(
-        RGB_TOG,  KC_NO,    KC_SCRL,  KC_PAUS,   KC_ESC,    KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_SAD,  RGB_SAI,  KC_MPRV,  KC_MPLY,   KC_MNXT, KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_NO,            RGB_TOG,
+        RGB_TOG,  KC_NO,    KC_SCRL,  KC_PAUS,   KC_ESC,    KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_SAD,  RGB_SAI,  KC_MPRV,  KC_MPLY,   KC_MNXT, KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,             KC_NO,
         KC_CALC,  KC_NO,    KC_NO,    KC_NO,     KC_NO,     KC_P1,    KC_P2,    KC_P3,    KC_P4,    KC_P5,    KC_P6,    KC_P7,    KC_P8,     KC_P9,   KC_P0,    KC_NO,      KC_NO,    KC_NO,              KC_NO,
         KC_PGUP,  KC_UP,    KC_HOME,  KC_NO,     TG(WIN_FN),KC_NO,    KC_MS_U,  KC_WH_U,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_NO,    KC_NO,      KC_NO,    KC_NO,              KC_NO,
         KC_LEFT,  KC_NO,    KC_RIGHT,            TG(WIN_FN),KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_NO,    KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,     KC_RIGHT,KC_NO,    KC_NO,                KC_NO,              KC_NO,
